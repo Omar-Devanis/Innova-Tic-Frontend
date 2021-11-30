64c9542ad0af622b28ead6cb42b0b151e57bce35
omar rodriguez <omardevanisrodriguez@gmail.com>
2021-11-30 16:15:38 -0500

    arreglitos en sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -3,11 +3,9 @@ import React from 'react'
 const Sidebar = () => {
     return (
         <nav class="sidebar">
-            <ul class="ul">
-                <li class="li"><button>Perfil</button></li>
-                <li class="li"><button>Proyectos</button></li>
-                <li class="li"><button>Usuarios</button></li>
-            </ul>
+            <button>Perfil</button>
+            <button>Proyectos</button>
+            <button>Usuarios</button>
         </nav>
 
     )
