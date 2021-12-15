b16cda9369e76b2da396ee60ae2777e81df1ac94
Felipe FV <feli13pe@gmail.com>
2021-12-15 02:32:53 -0500

    se crea ventana modal para perfil

--- src/pages/perfil/index.jsx
+++ src/pages/perfil/index.jsx
@@ -1,32 +1,50 @@
 import { useQuery } from '@apollo/client'
-import React, {useEffect} from 'react'
+import React, {useEffect,useState} from 'react'
 import {useUser} from '../../context/userContext';
 import { GET_USUARIO } from "../../graphql/usuarios/queries";
+import avatarU from "../../assets/img/avatarU.png"
+import Modal from '../../components/Modal'
 
 
 const IndexPerfil = () => {
+    const[estado,setEstado] = useState(false)
+    const{userData} = useUser()
+    const _id = userData._id
+    console.log(userData._id)
+    const {data:queryData,
+        error:queryError,
+        loading:queryLoading} = useQuery(GET_USUARIO,{
+        variables:{_id}
+    });
+
+    useEffect(() => {
+        console.log('data servidor', queryData)
+      }, [queryData])
     
-
-    const {data,error,loading} = useQuery(GET_USUARIO)
-
-    useEffect(()=>{
-        console.log("Estos son los datos",data)
-    },[data])
-
+    if (queryLoading) return <div>Cargando....</div>;
     return (
-        <div>
-            <div>
+        <>
+        <Modal estado={estado} setEstado={setEstado} title={"Actualizar Perfil"}></Modal>
+        <div className='perfil'>
+        
+            <div className='contenedorPerfil'>
                 <div className='headerPerfil'>
-                    <div className='picture'></div>
-                    <h3>Nombre Completo</h3>
+                    <div className='picture'>
+                        <img src={avatarU} alt='Usuario'/>
+                    </div>
+                    <h3>{(queryData.Usuario.nombre).toUpperCase()} {(queryData.Usuario.apellido).toUpperCase()}</h3>
                 </div>
                 <div className='bodyPerfil'>
-                    <h4>Identificacion</h4>
                     <h4>Correo</h4>
+                    <p>{queryData.Usuario.correo}</p>
                     <h4>Rol</h4>
-                </div>
+                    <p>{queryData.Usuario.rol}</p>
+                    <button onClick={()=>setEstado(!estado)}>Actualizar</button>
+                </div>  
             </div>
+            
         </div>
+        </>
     )
 }
 
